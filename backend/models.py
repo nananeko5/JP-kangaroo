6b5fcd356a1cd9a80aea3587f4b5de4669d3e4b8
Knpand <88275508+Knpand@users.noreply.github.com>
2021-10-26 14:43:49 +0900

    sample2

--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Card(db.Model):
+    __tablename__ = 'cards'
+    imgpath = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(15), nullable=False)
+    furigana = db.Column(db.String(30), nullable=False)
+    birthday = db.Column(db.DateTime, nullable=False)
+    favourite = db.Column(db.String(30), nullable=False)
+    skills = db.Column(db.String(30), nullable=False)
+
 class SpamModel(db.Model):
     __tablename__ = 'spam_table'
 
